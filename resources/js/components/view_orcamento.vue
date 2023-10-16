7a11be40a2b651f8aa625a3b17d0894e4d091553
DanielCostaGH <dfscs.costa@gmail.com>
2023-10-16 10:24:06 -0300

    FIX: Ajustes finais

--- resources/js/components/view_orcamento.vue
+++ resources/js/components/view_orcamento.vue
@@ -95,7 +95,7 @@ export default {
         formatarDataParaExibicao(data) {
             if (data) {
                 const date = new Date(data);
-                const dia = date.getDate().toString().padStart(2, '0');
+                const dia = (date.getDate() + 1).toString().padStart(2, '0');
                 const mes = (date.getMonth() + 1).toString().padStart(2, '0');
                 const ano = date.getFullYear();
                 return `${dia}/${mes}/${ano}`;
